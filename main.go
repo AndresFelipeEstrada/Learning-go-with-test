e02ef03b9f9435b06050ab30dd680afbf9e894b0
Andres Felipe Estrada <andresfe1396@gmail.com>
2025-02-23 13:55:36 -0500

    table driven tests

--- main.go
+++ main.go
@@ -1,4 +1,12 @@
 package main
 
+import "fmt"
+
 func main() {
+	figuras := []string{"circulo", "cuadrado", "triangulo", "rombo", "trapecio", "heptagono"}
+	figurasCopia := make([]string, len(figuras))
+	copy(figurasCopia, figuras)
+	figuras = append(figuras[:1], figuras[2:]...)
+	fmt.Println(figuras)
+	fmt.Println(figurasCopia)
 }
